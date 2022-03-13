e595ab41c4063fd29131d9a88de78e10ed11a00a
Jake Mullins <jmullins2003@outlook.com>
2022-03-12 19:47:49 -0700

    basic layout implemented

---
+++ src/components/GameRow.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="guess">
+    <div class="row" v-for="row in rows" :key="row">
+      <div class="function">
+        <div class="function-tile"></div>
+      </div>
+      <div class="argument">
+        <div class="argument-tile"></div>
+      </div>
+      <div class="argument">
+        <div class="argument-tile"></div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GameRow",
+  el: "#guess",
+  data: () => {
+    return {
+      rows: [1, 2, 3, 4, 5],
+    };
+  },
+  props: {
+    yellow: String,
+    green: Array,
+  },
+};
+</script>
+
+<style scoped>
+p {
+  padding: 0px;
+  margin: 0px;
+}
+
+.row {
+  grid-template-rows: repeat(6, 1fr);
+  box-sizing: border-box;
+  text-align: center;
+}
+
+.function {
+  display: inline-block;
+  width: 300px;
+  height: 100px;
+  background-color: #2b2b2b;
+  margin: 5px;
+}
+
+.argument {
+  display: inline-block;
+  width: 100px;
+  height: 100px;
+  background-color: #2b2b2b;
+  margin: 5px;
+}
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="game">
+    <div id="gameTitle">
+      <h1>Codele.js</h1>
+    </div>
+    <div id="board-container">
+      <div id="board" style="width: 700px; height: 420px">
+        <GameRow v-bind:yellow="One" v-bind:green="[4, 5, 6]" />
+        <GameRow v-bind:yellow="Two" v-bind:green="[4, 5, 6]" />
+        <GameRow v-bind:yellow="Three" v-bind:green="[4, 5, 6]" />
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import GameRow from "@/components/GameRow.vue";
+
+export default {
+  name: "GameView",
+  el: "#app",
+  data: function () {
+    return {
+      yellowString: "Poggers",
+    };
+  },
+  components: {
+    GameRow,
+  },
+};
+</script>
+
+<style scoped>
+#board-container {
+  display: flex;
+  justify-content: center;
+}
+
+#game-row {
+  display: grid;
+}
+</style>
